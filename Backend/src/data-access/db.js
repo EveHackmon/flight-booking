50fb79f74021309a090ac915cea7ddfc0da6ad05
DanielHouri5 <hdanielh50@gmail.com>
2025-06-23 10:14:57 +0300

    Complete Backend and initial Frontend

--- Backend/src/data-access/db.js
+++ Backend/src/data-access/db.js
@@ -2,7 +2,24 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false,
-});
+let sequelize;
+
+if (process.env.POSTGRES_URI) {
+  sequelize = new Sequelize(process.env.POSTGRES_URI, {
+    dialect: 'postgres',
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || 'flight_booking',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASS || 'pass',
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+}
+
+export { sequelize };
